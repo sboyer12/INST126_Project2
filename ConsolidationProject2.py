404f492d731a7439a2ee37320f52f00866c46b03
sboyer12@terpmail.umd.edu <sboyer12@terpmail.umd.edu>
2024-05-04 18:13:40 -0400

    This is my first commit that involves a project change. Contains aspect for a word bank

--- ConsolidationProject2.py
+++ ConsolidationProject2.py
@@ -1,3 +1,10 @@
 # Sebastian Boyer - Second Consolidation Project
 
-print("Hello World")
+import os
+import random
+
+# this is the function that I have created to come up with my secret word and my word bank
+def word_bank():
+    word_bank = ["raise", "scare", "point", "least", "trace"]
+    return random.choice(word_bank)
+
