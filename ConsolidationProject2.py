222645f433e0b504b6037719fc3f56ad8e8dbb71
sboyer12@terpmail.umd.edu <sboyer12@terpmail.umd.edu>
2024-05-04 18:29:39 -0400

    This is the next commit that I am doing. I have added some more functions.

--- ConsolidationProject2.py
+++ ConsolidationProject2.py
@@ -8,3 +8,21 @@ def word_bank():
     word_bank = ["raise", "scare", "point", "least", "trace"]
     return random.choice(word_bank)
 
+# this is going to be the function for the gameplay. It will include what is printed out and how the game goes along.
+def gameplay():
+    secretWord = word_bank # this is how the secret word is chosen
+    print("This is the word hunting game! \n")
+    numberOfPlayers = int(input("Enter number of players: "))
+    players = [f"Players {i + 1}" for i in range (numberOfPlayers)]
+
+    # now I am going to create the variables for how we are going to track letter and word guesses
+    letterGuesses = 0 # variable for letter guesses
+    wordGuesses = 0 # variable for word guesses
+
+
+    while True:
+        for player in players:
+            print(f"\n{player}'s turn:")
+            guess = input("Enter a letter guess: ").lower()
+
+    
